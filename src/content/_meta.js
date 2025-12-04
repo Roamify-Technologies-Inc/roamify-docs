f810035d6594a87c58d04b8b24a152c959862cd5
Torres Yang <app@getroamify.com>
2025-12-03 22:22:54 -0800

    Added exchange rates

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -1,15 +1,16 @@
 export default {
-  "index": "Introduction",
-  "authentication": "Authentication",
-  "esims": "eSIMs Module",
-  "webhooks": "Webhooks Module",
-  "gift-cards": "Gift Cards Module",
-  "affiliate": "Affiliate Module",
-  "health": "Health Check",
-  "about": "About eSIM API",
-  "contact": {
-    title: "Contact",
-    type: "page",
-    href: "https://partner.getroamify.com/sign-up",
-  }
+    "index": "Introduction",
+    "authentication": "Authentication",
+    "esims": "eSIMs Module",
+    "webhooks": "Webhooks Module",
+    "gift-cards": "Gift Cards Module",
+    "affiliate": "Affiliate Module",
+    "exchange-rates": "Exchange Rates",
+    "health": "Health Check",
+    "about": "About eSIM API",
+    "contact": {
+        title: "Contact",
+        type: "page",
+        href: "https://partner.getroamify.com/sign-up",
+    }
 }
---
+++ src/content/exchange-rates/_meta.js
@@ -0,0 +1,5 @@
+export default {
+    "overview": "Overview",
+    "get-currencies": "Get Currencies",
+    "get-exchange-rates": "Get Exchange Rates",
+}
